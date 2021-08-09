81b06056e634bd95d03207bff8cc85ee35443285
Takah1ro <tkhr.y0k0yama@gmail.com>
2021-08-09 17:09:46 +0900

    implement dummy completer

--- awsheol/core/client/cmdloop.py
+++ awsheol/core/client/cmdloop.py
@@ -1,4 +1,5 @@
 import asyncio
+import shlex
 
 from prompt_toolkit import PromptSession
 from prompt_toolkit.patch_stdout import patch_stdout
@@ -6,13 +7,28 @@ from prompt_toolkit.application import run_in_terminal
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.formatted_text import HTML
 
+class AWShCompleter(Completer):
+    def __init__(self, cli_menu):
+        self.cli_menu = cli_menu
+
+    def get_completions(self, document, complete_event):
+        word_before_cursor = document.get_word_before_cursor()
+        try:
+            cmd_line = [s.lower() for s in shlex.split(document.current_line)]
+        except ValueError:
+            pass
+        else:
+            if len(cmd_line):
+                pass
+
 class AWShell:
     def __init__(self, args):
         self.args = args
-        # self.completer = AWShCompleter(self)
+        self.current_context = self
+        self.completer = AWShCompleter(self)
         self.prompt_session = PromptSession(
             HTML("AWSh >> "),
-            # completer=self.completer,
+            completer=self.completer,
             complete_in_thread=True,
             complete_while_typing=True,
             auto_suggest=AutoSuggestFromHistory(),
