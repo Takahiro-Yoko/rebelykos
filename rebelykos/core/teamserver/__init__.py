fa6e0bed4746fa033e7262677d89c316bb67d956
Takah1ro <tkhr.y0k0yama@gmail.com>
2021-08-22 11:50:26 +0900

    fix indentation

--- rebelykos/core/teamserver/__init__.py
+++ rebelykos/core/teamserver/__init__.py
@@ -5,9 +5,9 @@ from rebelykos.core.ipcserver import IPCServer
 
 
 logging.basicConfig(
-        format=("%(asctime)s %(process)d %(threadName)s - [%(levelname)s] "
-                "%(filename)s: %(funcName)s - %(message)s"),
-        level=logging.DEBUG
+    format=("%(asctime)s %(process)d %(threadName)s - [%(levelname)s] "
+            "%(filename)s: %(funcName)s - %(message)s"),
+    level=logging.DEBUG
 )
 
 multiprocessing.set_start_method("fork")
