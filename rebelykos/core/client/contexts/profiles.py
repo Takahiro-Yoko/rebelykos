af29ee6a5a2dda4cb3da87c46dc562d406250131
Takah1ro <tkhr.y0k0yama@gmail.com>
2021-08-26 08:51:38 +0900

    update profile func

--- rebelykos/core/client/contexts/profiles.py
+++ rebelykos/core/client/contexts/profiles.py
@@ -1,3 +1,5 @@
+from terminaltables import SingleTable
+
 from rebelykos.core.client.utils import cmd, register_cli_cmds
 
 
@@ -20,6 +22,31 @@ class Profiles:
     def selected(self, data):
         self.prompt = f"(<ansired>{data['name']}</ansired>)"
         self._selected = data
+
+    @cmd
+    def set(self, name: str, value: str, response):
+        """
+        Set aws settings on the selected profile
+
+        Usage: set <name> <value> [-h]
+
+        Arguments:
+            name name
+            value value
+        """
+
+    @cmd
+    def info(self, response):
+        """
+        Show detailed information of the selected profile
+
+        Usage: info [-h]
+        """
+        table_data = [["profile", "access_key_id", "secret_access_key",
+                       "region", "session_token"]]
+        table_data.append([response.result[k] for k in table_data[0]])
+        table = SingleTable(table_data, title=self.selected["name"])
+        print(table.table)
 
     @cmd
     def use(self, name: str, response):
--- rebelykos/core/teamserver/contexts/profiles.py
+++ rebelykos/core/teamserver/contexts/profiles.py
@@ -1,51 +1,59 @@
 import logging
 
 from rebelykos.core.teamserver.db import AsyncRLDatabase, RLDatabase
-from rebelykos.core.teamserver.loader import Loader
-from rebelykos.core.utils import get_path_in_package
+from rebelykos.core.utils import CmdError
 
 
-class Profiles(Loader):
+class Profiles:
     name = "profiles"
     description = "Profiles menu"
 
     def __init__(self, teamserver):
         self.teamserver = teamserver
-        self.profiles = []
+        self.profiles = set()
         self.selected = None
-        super().__init__(
-            type="profile",
-            paths=[get_path_in_package("core/teamserver/profiles")]
-        )
 
     def get_selected(self):
         if self.selected:
-            return dict(self.selected)
+            return self.selected
+
+    def info(self):
+        if not self.selected:
+            raise CmdError("No profile selected")
+        return self.selected
+
+    def set(self, name: str, value: str):
+        if not self.selected:
+            raise CmdError("No profile selected")
+        try:
+            self.selected[name] = value
+        except KeyError:
+            raise CmdError(f"Unknown option '{name}'")
 
     def use(self, name: str):
         with RLDatabase() as db:
             for p in db.get_profiles():
                 if name == p[1]:
-                    self.selected = p
-                    return {**{x: p[i] for i, x
-                               in enumerate(("profile",
-                                             "access_key_id",
-                                             "secret_access_key",
-                                             "session_token",
-                                             "region"),
-                                            1)},
-                            "name": name}
+                    self.selected = {**{x: p[i] for i, x
+                                     in enumerate(("profile",
+                                                   "access_key_id",
+                                                   "secret_access_key",
+                                                   "session_token",
+                                                   "region"),
+                                                  1)},
+                                     "name": name}
+                    return self.selected
         logging.debug(f"profile {name} does not exist")
-        return {"profile": name,
-                "access_key_id": "",
-                "secret_access_key": "",
-                "session_token": "",
-                "region": "",
-                "name": name}
+        self.selected = {"profile": name,
+                         "access_key_id": "",
+                         "secret_access_key": "",
+                         "session_token": "",
+                         "region": "",
+                         "name": name}
             
 
     def __iter__(self):
-        yield ("loaded", len(self.loaded))
+        yield ("profiles", len(self.profiles))
 
     def __str__(self):
         return self.__class__.__name__.lower()
