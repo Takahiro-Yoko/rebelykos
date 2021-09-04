064e8e5334196f96c05a7d172ac4c44bbb1295ac
Takah1ro <tkhr.y0k0yama@gmail.com>
2021-09-05 08:22:50 +0900

    remove comments

--- rebelykos/core/teamserver/module.py
+++ rebelykos/core/teamserver/module.py
@@ -22,23 +22,8 @@ class Module:
         try:
             return res.RESULT, func(**kwargs)
         except botocore.exceptions.ClientError as e:
-            # func_args = f"{'(' + str(kwargs) + ')' if kwargs else ''}"
-            func_args = f"({kwargs or ''})"
-            return res.BAD, (f'When calling {func.__name__}'
-                             f'{func_args}: {e.response["Error"]["Code"]}')
-            # ecode = e.response["Error"]["Code"]
-            # key = self['profile']['aws_access_key_id']
-            # if ecode == "InvalidClientTokenId":
-            #     msg = f"{func}: The AWS key is invalid"
-            # elif ecode == "AccessDenied":
-            #     msg = f"{func}: AccessDenied"
-            # elif ecode == "SubscriptionRequiredException":
-            #     msg = (f"{key} : has permissions but isn't signed up for"
-            #            " service - usually means you have a root account")
-            # else:
-            #     msg = f"{func}: Unexpected error ({e})"
-            # return res.BAD, msg
-
+            return res.BAD, (f'When calling {func.__name__}({kwargs or ""})'
+                             f': {e.response["Error"]["Code"]}')
 
     def __getitem__(self, key):
         for k, v in self.options.items():
