0bf53a25e5102be16005c6eb862e4dd796c08dbb
Takah1ro <tkhr.y0k0yama@gmail.com>
2021-09-09 12:37:17 +0900

    update .gitignore

---
+++ tests/check_monitoring/main.tf
@@ -0,0 +1,82 @@
+resource "aws_iam_user" "rebelykos" {
+  name = "rebelykos"
+}
+
+resource "aws_iam_access_key" "rebelykos" {
+  user = aws_iam_user.rebelykos.name
+  pgp_key = data.local_file.pgp_key.content_base64
+}
+
+resource "aws_iam_user_policy" "rebelykos_ro" {
+  name = "rebelykos"
+  user = aws_iam_user.rebelykos.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "cloudtrail:DescribeTrails",
+        "guardduty:ListDetectors",
+        "access-analyzer:ListAnalyzers"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_cloudtrail" "rebelykos" {
+  name                          = "rebelykos"
+  s3_bucket_name                = aws_s3_bucket.rebelykos.id
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = true
+}
+
+resource "aws_s3_bucket" "rebelykos" {
+  bucket        = "rebelykos"
+  force_destroy = true
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::rebelykos"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::rebelykos/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
+
+output "key" {
+  value = aws_iam_access_key.rebelykos.id
+}
+output "secret" {
+  value = aws_iam_access_key.rebelykos.encrypted_secret
+}
---
+++ tests/check_monitoring/variables.tf
@@ -0,0 +1,3 @@
+data "local_file" "pgp_key" {
+  filename = "/home/taka/.gnupg/public-key-binary.gpg"
+}
