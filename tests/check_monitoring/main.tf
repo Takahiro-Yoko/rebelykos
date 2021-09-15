52d006bf7cbb3d690534d86a48476eb22b7657d4
Takah1ro <tkhr.y0k0yama@gmail.com>
2021-09-16 08:24:05 +0900

    to use outputs.tf

--- tests/check_monitoring/main.tf
+++ tests/check_monitoring/main.tf
@@ -77,10 +77,3 @@ POLICY
 resource "aws_accessanalyzer_analyzer" "rebelykos" {
   analyzer_name = "rebelykos"
 }
-
-output "key" {
-  value = aws_iam_access_key.rebelykos.id
-}
-output "secret" {
-  value = aws_iam_access_key.rebelykos.encrypted_secret
-}
