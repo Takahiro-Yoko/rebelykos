5bcee2d991b3dacc3797d43feba2976062c91167
Takah1ro <tkhr.y0k0yama@gmail.com>
2021-09-22 07:58:13 +0900

    Change directory name

--- tests/iam_get_policies/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "key" {
-  value = aws_iam_access_key.rebelykos.id
-}
-
-output "secret" {
-  value = aws_iam_access_key.rebelykos.encrypted_secret
-}
-
--- tests/iam_get_policies/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "region" {
-  default = "ap-northeast-1"
-}
-
-variable "pgp_key" {
-  default = "~/.gnupg/public-key-binary.gpg"
-}
