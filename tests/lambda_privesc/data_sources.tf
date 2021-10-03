39d54590dc2f4406be66a0bb71a8cc7f500d57f6
Takah1ro <tkhr.y0k0yama@gmail.com>
2021-10-03 20:46:37 +0900

    Add test tf for lambda_privesc

---
+++ tests/lambda_privesc/data_sources.tf
@@ -0,0 +1,3 @@
+data "aws_iam_policy" "admin-access" {
+  arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
---
+++ tests/lambda_privesc/main.tf
@@ -0,0 +1,55 @@
+resource "aws_iam_user" "rebelykos" {
+  name = "rebelykos"
+}
+
+resource "aws_iam_access_key" "rebelykos" {
+  user = aws_iam_user.rebelykos.name
+  pgp_key = filebase64(var.pgp_key)
+}
+
+resource "aws_iam_user_policy" "rebelykos" {
+  name = "rebelykos"
+  user = aws_iam_user.rebelykos.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "iam:PassRole",
+        "lambda:CreateFunction",
+        "lambda:InvokeFunction",
+        "lambda:DeleteFunction"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "rebelykos" {
+  name = "rebelykos"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "rebelykos" {
+  role = aws_iam_role.rebelykos.name
+  policy_arn = data.aws_iam_policy.admin-access.arn
+}
